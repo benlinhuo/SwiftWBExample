0daa901d0a8d35be0038845a4232cb7095677697
huobenlin <huobenlin@zhongan.com>
2018-09-18 10:03:00 +0800

    swift init

---
+++ WBExampleSwift/WBExampleSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WBExampleSwift
+//
+//  Created by benlinhuo on 2018/9/17.
+//  Copyright © 2018年 benlinhuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
